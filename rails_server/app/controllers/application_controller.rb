2b38a310c4abcd536ca3afe463be6f464b3a1e04
Andrew Cates <amcates@gmail.com>
2019-01-17 22:05:54 -0500

    messing around with authentication

--- rails_server/app/controllers/application_controller.rb
+++ rails_server/app/controllers/application_controller.rb
@@ -1,2 +1,5 @@
 class ApplicationController < ActionController::Base
+  #require 'dotenv'
+  #Dotenv.load(Rails.root.join(".env"))
+  #http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
 end
