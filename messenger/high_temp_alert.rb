59205b3ed6f6383d45a61628de9834d72217cea1
Andrew Cates <amcates@gmail.com>
2019-12-31 19:00:09 -0500

    adding high temp twilio messenger

---
+++ messenger/high_temp_alert.rb
@@ -0,0 +1,32 @@
+require 'twilio-ruby'
+require 'dotenv'
+Dotenv.load("twilio.env")
+
+location = '/sys/bus/w1/devices/28-021892458db6/w1_slave'
+
+lines = IO.readlines(location)
+last_line = lines.last
+
+raw_temp = last_line.split("=").last.gsub("\n", "")
+temp_readout = (raw_temp.to_f/1000.0 * 1.8) + 32
+temp_readout += 2 # adding offset
+
+current_temp = temp_readout.round(2)
+
+# only send SMS message if temp is greater than 105.0
+if current_temp >= 105.0
+  message = "Current Temp: #{current_temp}°F"
+
+  account_sid = ENV['SID']
+  auth_token = ENV['AUTH_TOKEN']
+  sms_to = ENV['SMS_TO']
+  sms_from = ENV['SMS_FROM']
+
+  @client = Twilio::REST::Client.new account_sid, auth_token
+
+  @client.api.account.messages.create(
+    from: sms_from,
+    to: sms_to,
+    body: message
+  )
+end
